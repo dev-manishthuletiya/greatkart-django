e0f8fe69733b4fc2104a7efd5ba7f8da2018829e
dev-manishthuletiya <manishthuletiya28@gmail.com>
2024-04-12 20:51:55 +0530

    handled cart and checkout page for logged in users

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -153,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'manishthuletiya2801@gmail.com'
-EMAIL_HOST_PASSWORD = 'emle kpje tmmh wxhl'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS = True
