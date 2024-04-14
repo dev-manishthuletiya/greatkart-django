a0c8b4fb03cf2402bd4526063f59aa30034d0ac0
dev-manishthuletiya <manishthuletiya28@gmail.com>
2024-04-14 08:26:11 +0530

    Product Order and Payment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -153,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'manishthuletiya2801@gmail.com'
-EMAIL_HOST_PASSWORD = '******'
+EMAIL_HOST_PASSWORD = 'emle kpje tmmh wxhl'
 EMAIL_USE_TLS = True
